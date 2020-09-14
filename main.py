c85ed7e37d636ec657dc581d54b2d74d73a4ec69
FedericoV <vaglicafederico95@gmail.com>
2020-09-14 17:52:46 +0200

    Declaring Variables

---
+++ main.py
@@ -0,0 +1,27 @@
+print('Welcome to my first game')
+
+name = input('Whath is your name? ')
+age = input('What  is your age? ')
+
+print('Hello' ,name , 'your age is' ,age ,'years old ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+ Data type:
+str     'hello', "World" , "89"
+int       5, -90 , 1000 , -43
+float        5.3 ,3.5, 6.7 ,-100.0
+bool      true || false
+'''
