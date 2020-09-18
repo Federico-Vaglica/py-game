98a2c72c0b5d1ec9c73f53d7ac54de43cd517e71
FedericoV <vaglicafederico95@gmail.com>
2020-09-18 09:31:58 +0200

    Nested if else

--- main.py
+++ main.py
@@ -1,9 +1,39 @@
 print('Welcome to my first game')
 
 name = input('Whath is your name? ')
-age = input('What  is your age? ')
+age = int(input('What  is your age? '))
+health = 10
 
-print('Hello' ,name , 'your age is' ,age ,'years old ')
+
+if age >= 18:
+    print('you are old enought')
+
+    wants_to_play=input('Do you want to play?  ').lower()
+    
+    if wants_to_play == 'yes':
+        print('Lets play!')
+        print('You starting the game with', health,'health')
+
+        left_or_right = input('Ook first choise... Do you want to go left or right (left/right)? ')
+
+        if left_or_right == 'left':
+            ans = input('Nice , you follow the path and reach a lake ... Do you swim across or go around (across/around) ?')
+
+            if ans == 'around':
+                print('You went around the lake and reached the other side of the lake')
+            elif ans == 'across':
+                print('You managed to get across the lake ,  but were bit by a fish and lost 5 health ')
+                health -= 5
+            else:
+                print('You lost')        
+        else:
+            print('You fell down and lost') 
+    else:
+        print('See you later !')
+else:
+    print('You are not old enough to play')
+
+
 
 
 
