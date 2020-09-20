1e057be80172a104db1d1169a2f4c60d4bc92306
FedericoV <vaglicafederico95@gmail.com>
2020-09-20 09:44:23 +0200

    Game Done

--- main.py
+++ main.py
@@ -1,57 +1,51 @@
-print('Welcome to my first game')
+print("Welcome to my first game!")
+name = input("What is your name? ")
+age = int(input("What is your age? "))
 
-name = input('Whath is your name? ')
-age = int(input('What  is your age? '))
 health = 10
 
-
 if age >= 18:
-    print('you are old enought')
+    print("You are old enough to play!")
 
-    wants_to_play=input('Do you want to play?  ').lower()
-    
-    if wants_to_play == 'yes':
-        print('Lets play!')
-        print('You starting the game with', health,'health')
+    wants_to_play = input("Do you want to play? ").lower()
+    if wants_to_play == "yes":
+        print("You are staring with", health, "health")
+        print("Let's play!")
 
-        left_or_right = input('Ook first choise... Do you want to go left or right (left/right)? ')
+        left_or_right = input("First choice... Left or Right (left/right)? ")
+        if left_or_right == "left":
+            ans = input("Nice, you follow the path and reach a lake... Do you swim across or go around (across/around)? ")
 
-        if left_or_right == 'left':
-            ans = input('Nice , you follow the path and reach a lake ... Do you swim across or go around (across/around) ?')
-
-            if ans == 'around':
-                print('You went around the lake and reached the other side of the lake')
-            elif ans == 'across':
-                print('You managed to get across the lake ,  but were bit by a fish and lost 5 health ')
+            if ans == "around":
+                print("You went around and reached the other side of the lake.")
+            elif ans == "across":
+                print("You managed to get across, but were bit by a fish and lost 5 health.")
                 health -= 5
+
+            ans = input("You notice a house and a river. Which do you go to (river/house)? ")
+            if ans == "house":
+                print("You go to the house and are greeted by the owner... He doesn't like you and you lose 5 health")
+                health -= 5
+
+                if health <= 0:
+                    print("You now have 0 health and you lost the game...")
+                else:
+                    print("You have survived... You win!")
+
             else:
-                print('You lost')        
+                print("You fell in the river and lost...")
+
+
         else:
-            print('You fell down and lost') 
+            print("You fell down and lost...")
+
     else:
-        print('See you later !')
+        print("Cya...")
 else:
-    print('You are not old enough to play')
+    print("You are not old enough to play...")
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-'''
- Data type:
-str     'hello', "World" , "89"
-int       5, -90 , 1000 , -43
-float        5.3 ,3.5, 6.7 ,-100.0
-bool      true || false
-'''
